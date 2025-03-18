6732189a542432326c6d872df966d219206cbc6d
8WhyIAmHere8 <alisadozorceva@gmail.com>
2025-03-18 15:09:18 +0000

    server

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ app.get("/", (req, res) => {
   res.send("Fire Warden Tracker API is running!");
 });
 
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
